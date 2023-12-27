ab6a47e8435294731bd0e4e920ab9da01e5892f7
alma tamir <alma.tamir@runi.ac.il>
2023-12-27 16:06:08 +0200

    good 34

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,25 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		String s = num + " is a perfect number since " + num + " = 1";
+		int sum = 1;
+		for(int i = 2; i < num; i++)
+		{
+			
+			   if(num % i == 0)
+			  {
+				s += " + "+i;
+				sum += i;
+
+			  }
+
+			
+		}
+		if (sum!=num)
+		{
+				System.out.println(num+ " is not a perfect number");
+		}
+			else System.out.println(s);
 	}
 }
