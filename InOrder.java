cb1810815cbab19816ea2c3ac1c6c53dab66836c
alma tamir <alma.tamir@runi.ac.il>
2023-12-27 16:21:16 +0200

    g22

--- InOrder.java
+++ InOrder.java
@@ -4,13 +4,13 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-        int num1;
+        int num1 =0;
 		int num2 = 0;
-		int stop=0;
+		int stop = 0;
 		while (stop==0)
 		{
 		    num1 = (int)((Math.random() * 10));
-		    if (num1>num2)
+		    if (num1>=num2)
 		    	System.out.println(num1);
 		    else stop=1;
 		    num2=num1;
