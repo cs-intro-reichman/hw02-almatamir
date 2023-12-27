2d75cb2c3092ce569a04c6b340e18756ad61861b
alma tamir <alma.tamir@runi.ac.il>
2023-12-27 16:45:18 +0200

    g5

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,28 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		for (int i=0; i<num; i++)
+		    {
+		    	if (i%2==0)
+		    	{
+		    		 for (int j=0; j<num; j++)
+		        	{
+			          System.out.print("* ");
+			        }
+			    }
+			 
+		    	else
+		    	{
+                    for (int j=0; j<num; j++)
+		        	{
+			          System.out.print(" *");
+			        }
+			    }
+			System.out.println();
+			}
+		   
+			
+	    
 	}
 }
