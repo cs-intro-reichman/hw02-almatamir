14e88b657513734091834e4b91ab8cde1d8706ae
alma tamir <alma.tamir@runi.ac.il>
2023-12-30 20:23:22 +0200

    done 6

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,37 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = (int)(Math.random() * 10 + 1);
+		int countk=0;
+		boolean isgirl= true;
+		boolean isboy= true;
+		int stop=0;
+		String s="";
+		while(stop==0)
+		{
+
+			if (num>5)
+			{
+				s+=" g ";
+				isgirl=false;
+				System.out.print("g ");
+			}
+			else 
+			{
+                s+=" b ";
+                isboy=false;
+                System.out.print("b ");
+			}
+			countk++;
+			if (isboy==false && isgirl ==false)
+			{
+				System.out.println();
+				System.out.println("You made it... and you now have "+ countk+ " children.");
+				stop=1;
+			}
+			else num = (int)(Math.random() * 10 + 1);
+
+
+		}
 	}
 }
